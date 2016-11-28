2d3fc3ef645d0f7edd7c595caf3d3c9b86cbea83
Dan Woods <daniel.p.woods@gmail.com>
2016-11-28 15:27:51 -0500

    adding books and such

--- src/ratpack/ratpack.groovy
+++ src/ratpack/ratpack.groovy
@@ -1,14 +1,33 @@
+import g3summit.Book
+import g3summit.BookService
+import g3summit.DefaultBookService
+
 import static ratpack.groovy.Groovy.ratpack
 import ratpack.registry.Registry
 
 ratpack {
-  handlers {
-    all {
-      next(Registry.single("Hello World"))
-    }
 
-    all {
-      render get(String)
+  bindings {
+    bind(BookService, DefaultBookService)
+  }
+
+  handlers {
+
+    prefix("book/:isbn") {
+
+      all { BookService bookService ->
+        def isbn = allPathTokens.isbn
+        def book = bookService.getBook(isbn)
+        next(Registry.single(book))
+      }
+
+      get("title") {
+        response.send(get(Book).title)
+      }
+
+      get("author") { Book book ->
+        response.send(book.author)
+      }
     }
   }
 }
