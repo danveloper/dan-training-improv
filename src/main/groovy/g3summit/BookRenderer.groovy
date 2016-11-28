307ade7aee8f5c2bfa907b5a254f4a46686d3ee5
Dan Woods <daniel.p.woods@gmail.com>
2016-11-28 17:11:34 -0500

    answer for rendering a book many ways

--- src/main/groovy/g3summit/BookRenderer.groovy
+++ src/main/groovy/g3summit/BookRenderer.groovy
@@ -3,6 +3,7 @@ package g3summit
 import ratpack.handling.Context
 import ratpack.render.RendererSupport
 
+import static ratpack.handlebars.Template.handlebarsTemplate
 import static ratpack.jackson.Jackson.json
 
 class BookRenderer extends RendererSupport<Book> {
@@ -16,6 +17,12 @@ class BookRenderer extends RendererSupport<Book> {
     //  - Look at ratpack.handlebars.Template.handlebarsTemplate
     //  - Look at Context#byContent
 
-    ctx.render(json(book))
+    def tmpl = handlebarsTemplate("book", book, "text/html")
+
+    ctx.byContent { c -> c
+        .json { ctx.render(json(book)) }
+        .html { ctx.render(tmpl) }
+        .noMatch("text/html")
+    }
   }
 }
